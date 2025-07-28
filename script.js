d5978d26be823e36e0f5f6f790b8683970552911
HT189 <harry.thavaganeshan@gmail.com>
2025-07-28 22:31:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ fetch("https://script.google.com/macros/s/AKfycbzvWvW1IwKRMRho_9p1rMTJYzf91i5T6n
       card.className = "border rounded-2xl shadow p-4 bg-white";
 
       card.innerHTML = `
-        <img src="${item.image}" alt="${item.title}" class="w-full h-64 object-cover rounded-xl mb-4">
+        <img src="${item.image}" alt="${item.title}" style="width:100%; max-height:300px; object-fit:cover; border-radius:1rem; margin-bottom:1rem;">
         <h2 class="text-xl font-semibold">${item.title}</h2>
         <p class="text-sm text-gray-600">${item.description}</p>
         <p class="mt-2 text-lg font-bold text-pink-600">${item.price}</p>
-      `;
+    `;
+
 
       grid.appendChild(card);
     });
